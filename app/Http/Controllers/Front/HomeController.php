d22a8c5d404224db281bdb7309cd64ff93f2e030
Akbor Hossain <testakbor@gmail.com>
2020-08-19 11:21:29 +0600

    added category

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers\Front;
 
+use DB;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('front.home');
+
+        $categories=DB::table('term_taxonomy')
+        ->join('terms', 'terms.term_id', '=', 'term_taxonomy.term_id')
+        ->where('term_taxonomy.taxonomy','product_cat')
+        ->where('terms.status',1)
+        ->select('term_taxonomy.*','terms.name','terms.status')
+        ->orderBy('term_taxonomy.term_taxonomy_id','desc')
+        ->get();
+        return view('front.home',compact('categories'));
     }
 }
