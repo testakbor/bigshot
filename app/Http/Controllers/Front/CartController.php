343bd3b39d56521f28039e3200ece506e83e33f0
saifulislamsappu <saifulislam3844@gmail.com>
2020-09-06 15:41:55 +0600

    order_item details add

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -19,11 +19,11 @@ class CartController extends Controller
     {
         $info= Cart::getContent();
         
-    	return view('front.cart',compact('info'));
+        return view('front.cart',compact('info'));
     }
 
     public function addCart(Request $request){    
-        
+
         Cart::add(array(
             array(
                 'id' => $request->id,
@@ -32,9 +32,9 @@ class CartController extends Controller
                 'name' => $request->name,
                 'options' => array()
             )
-                )
-            );
-      return redirect(route('product-page',$request->id));
+        )
+    );
+        return redirect(route('product-page',$request->id));
     }
     
     public function index()
@@ -45,7 +45,7 @@ class CartController extends Controller
     public function checkout(Request $request){
         $post_date=date('Y-m-d H:i:s');
         $post_date_gmt=date('Y-m-d H:i:s',strtotime('+6 hour'));
-         $order=array(
+        $order=array(
             'post_title'=>'Order'.date('Y-m-d H:i:s'),
             'post_status'=>'on-hold',
             'post_name'=>'order-'.date('M-d-Y-Hm'),
@@ -61,15 +61,210 @@ class CartController extends Controller
 
         $order_post=array(
             'post_id'=>$order_id,
+            'meta_key'=>'_shipping_address_1',
+            'meta_value'=>$request->address1,
+        );
+        DB::table('postmeta')->insert($order_post);
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_shipping_country',
+            'meta_value'=>$request->_shipping_country,
+        );
+        DB::table('postmeta')->insert($order_post); 
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_shipping_last_name',
+            'meta_value'=>$request->lastName,
+        );
+        DB::table('postmeta')->insert($order_post);  
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_shipping_first_name',
+            'meta_value'=>$request->firstName,
+        );
+        DB::table('postmeta')->insert($order_post); 
+        $order_post=array(
+            'post_id'=>$order_id,
             'meta_key'=>'_billing_postcode',
-            'meta_value'=>'khilkhat',
+            'meta_value'=>$request->zip,
+        );
+        DB::table('postmeta')->insert($order_post); 
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_state',
+            'meta_value'=>$request->state,
         );
         DB::table('postmeta')->insert($order_post); 
 
-        return redirect(route('order.success'));
-        // dd($order_post);
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_city',
+            'meta_value'=>$request->city,
+        );
+        DB::table('postmeta')->insert($order_post); 
+        
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_address_2',
+            'meta_value'=>$request->address2,
+        );
+        DB::table('postmeta')->insert($order_post); 
 
-    }
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_address_1',
+            'meta_value'=>$request->address1,
+        );
+        DB::table('postmeta')->insert($order_post);   
+
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_address_2',
+            'meta_value'=>$request->address2,
+        );
+        DB::table('postmeta')->insert($order_post); 
+
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_country',
+            'meta_value'=>$request->_billing_country,
+        );
+        DB::table('postmeta')->insert($order_post);
+
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_phone',
+            'meta_value'=>$request->phone,
+        );
+        DB::table('postmeta')->insert($order_post);
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_email',
+            'meta_value'=>$request->_billing_email,
+        );
+        DB::table('postmeta')->insert($order_post);
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_company',
+            'meta_value'=>$request->_billing_company,
+        );
+        DB::table('postmeta')->insert($order_post); 
+
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_last_name',
+            'meta_value'=>$request->lastName,
+        );
+        DB::table('postmeta')->insert($order_post); 
+
+        $order_post=array(
+            'post_id'=>$order_id,
+            'meta_key'=>'_billing_first_name',
+            'meta_value'=>$request->firstName,
+        );
+     
+        DB::table('postmeta')->insert($order_post); 
+
+        $info= Cart::getContent();
+
+        foreach ($info as  $value) {
+           $order_item=array(
+            'order_item_name'=>$value->name,
+            'order_item_type'=>'line-item',
+            'order_id'=>$order_id,
+        );
+
+        DB::table('order_items')->insert($order_item); 
+ 
+
+       }
+
+        foreach ($info as $item){
+
+        $order_item=array(
+            'order_item_name'=>$item->name,
+            'order_item_type'=>'line-item',
+            'order_id'=>$order_id,
+        );
+
+        $order_item_id=DB::table('order_items')->insertGetId($order_item);
+
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_tax_class',
+            'meta_value'=>'',
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_qty',
+            'meta_value'=>$item->quantity,
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_product_id',
+            'meta_value'=>'',
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_variation_id',
+            'meta_value'=>'',
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_line_subtotal',
+            'meta_value'=>$item=Cart::getSubTotal(),
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_line_total',
+            'meta_value'=>$item=Cart::getTotal(),
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);  
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_line_subtotal_tax',
+            'meta_value'=>'',
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);   
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_line_tax',
+            'meta_value'=>'',
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+
+        // $_line_tax_data=date('Y-m-d H:i:s');
+        $_line_tax_data_gmt=date('Y-m-d H:i:s',strtotime('+6 hour'));
+
+           $order_item_details=array(
+            'order_item_id'=>$order_item_id,
+            'meta_key'=>'_line_tax_data',
+            'meta_value'=>$_line_tax_data_gmt,
+        );
+           DB::table('order_itemmeta')->insert($order_item_details);
+
+        }
+       
+
+        // return redirect(route('order.success'));
+       // dd($order_post);
+       dd($order_item_details);
+
+   }
 
     /**
      * Show the form for creating a new resource.
@@ -78,7 +273,7 @@ class CartController extends Controller
      */
     public function create(Request $request)
     {
-        
+
     }
 
     /**
