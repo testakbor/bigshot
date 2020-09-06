32bee26052cd159ba588540771322975fd2ef38d
Akbor hossain <testakbor@gmail.com>
2020-09-06 18:43:47 +0600

    alignemtn

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['namespace'=>'Front'],function(){
+    Route::group(['namespace'=>'Front'],function(){
         Route::get('/', 'HomeController@index')->name('home');
         Route::get('/product-page/{id}','PageController@productView')->name('product-page');
         Route::get('/pickup-tab','PageController@pickupTab')->name('pickup.tab');
@@ -44,27 +44,26 @@ Route::group(['namespace'=>'Front'],function(){
 
     Route::group(['namespace'=>'User'],function(){
         Route::get('/order-list','OrderController@OrderList')->name('order.list');
-
     });
 
-Route::group(['namespace'=>'Admin'],function(){
-    Route::GET('admin-login','Auth\LoginController@showLoginForm')->name('admin.login');
-    Route::POST('admin-login','Auth\LoginController@login');
-    Route::get('admin/home','HomeController@index')->name('admin.home');
+    Route::group(['namespace'=>'Admin'],function(){
+        Route::GET('admin-login','Auth\LoginController@showLoginForm')->name('admin.login');
+        Route::POST('admin-login','Auth\LoginController@login');
+        Route::get('admin/home','HomeController@index')->name('admin.home');
 
-    Route::resource('admin/category','CategoryController');
-    Route::resource('admin/tag','TagController');
-    Route::resource('admin/brand','BrandController');
-    Route::resource('admin/attribute','AttributeController');
-    Route::resource('admin/product','ProductController');
-    Route::resource('admin/order','OrderController');
+        Route::resource('admin/category','CategoryController');
+        Route::resource('admin/tag','TagController');
+        Route::resource('admin/brand','BrandController');
+        Route::resource('admin/attribute','AttributeController');
+        Route::resource('admin/product','ProductController');
+        Route::resource('admin/order','OrderController');
 
-    Route::get('admin/attributeValue/{id}','AttributeController@attributeValue')->name('attribute.attributeValue');
-    Route::POST('admin/attributeValueSave','AttributeController@attributeValueSave')->name('attributeValue.store');
-    Route::get('admin/attributeValueEdit/{id}','AttributeController@attributeValueEdit')->name('attributeValue.edit');
-    Route::PATCH('admin/attributeValueUpdate/{id}','AttributeController@attributeValueUpdate')->name('attributeValue.update');
+        Route::get('admin/attributeValue/{id}','AttributeController@attributeValue')->name('attribute.attributeValue');
+        Route::POST('admin/attributeValueSave','AttributeController@attributeValueSave')->name('attributeValue.store');
+        Route::get('admin/attributeValueEdit/{id}','AttributeController@attributeValueEdit')->name('attributeValue.edit');
+        Route::PATCH('admin/attributeValueUpdate/{id}','AttributeController@attributeValueUpdate')->name('attributeValue.update');
 
-    Route::get('admin/product/arttibuteValue/{id}','ProductController@attributeValue')->name('product.arttibuteValue');
-    Route::POST('admin/product/store','ProductController@store')->name('product.store');
-});
+        Route::get('admin/product/arttibuteValue/{id}','ProductController@attributeValue')->name('product.arttibuteValue');
+        Route::POST('admin/product/store','ProductController@store')->name('product.store');
+    });
 
