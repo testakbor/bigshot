55de483f2687e3b51ad7a601fffe2511cfb00073
saifulislamsappu <saifulislam3844@gmail.com>
2020-09-14 17:39:07 +0600

    s

--- app/Http/Controllers/Front/PageController.php
+++ app/Http/Controllers/Front/PageController.php
@@ -51,18 +51,16 @@ class PageController extends Controller
 	}
 	public function categoryProduct($id)
 	{
-		$categories=DB::table('term_taxonomy')
-        ->join('terms', 'terms.term_id', '=', 'term_taxonomy.term_id')
+		$categories=DB::table('term_relationships')
+        ->join('posts', 'posts.ID', '=', 'term_relationships.object_id')
         ->where('term_taxonomy.taxonomy','product_cat')
         ->where('terms.status',1)
         ->select('term_taxonomy.*','terms.name','terms.status')
         ->orderBy('term_taxonomy.term_taxonomy_id','desc')
         ->get();
-
         // product 
         $products=Post::where('post_type','product')
         ->where('post_status','publish')
-   
         ->get();
 	    return view('front.Categories',compact('categories','products'));
 	}
