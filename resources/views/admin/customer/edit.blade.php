3e3fde63f1a18e2cde767942e3f4bbfa74e41ff9
parthokar90 <parthokar90@gmail.com>
2020-09-15 17:51:30 +0600

    c

--- resources/views/admin/customer/edit.blade.php
+++ resources/views/admin/customer/edit.blade.php
@@ -6,12 +6,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Tag List</h1>
+            <h1>Customer Edit</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{route('admin.home')}}">Home</a></li>
-              <li class="breadcrumb-item active">Tag List</li>
+              <li class="breadcrumb-item active">Customer Edit</li>
             </ol>
           </div>
         </div>
