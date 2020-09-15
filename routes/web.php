c75fa6f325cf9dff4d896c3f25c5479ac20343cd
Akbor Hossain <testakbor@gmail.com>
2020-09-15 22:03:29 +0600

    user profile

--- routes/web.php
+++ routes/web.php
@@ -59,10 +59,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
     Route::group(['namespace'=>'User'],function(){
         Route::resource('/order-list','OrderController');
+        Route::resource('user/profile','UserController');
     }); 
        
     Route::group(['namespace'=>'Search'],function(){
         Route::post('/search','SearchController@nSearch')->name('search');
+       
     });
 
     Route::group(['namespace'=>'Admin'],function(){
