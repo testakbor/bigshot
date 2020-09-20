7a85223def2211cc83c50d448d64bbea3aa2d273
saifulislamsappu <saifulislam3844@gmail.com>
2020-09-20 12:46:36 +0600

    print page

--- resources/views/admin/order/pendingOrder_print.blade.php
+++ resources/views/admin/order/pendingOrder_print.blade.php
@@ -1,98 +1,57 @@
-<?php 
-use App\Model\front\Order_item;
-?>
-@extends('admin.layouts.master')
-@section('content')
-<div class="content-wrapper" style="min-height: 1203.6px;">
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-    <!-- Main content -->
-    <section class="content mt-5">
-      <div class="container">
-        <div class="card">
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
 
-          
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
 
-          <div class="card-body">
-           
 
-            <div class="table-responsive-sm">
-              <table class="table table-striped">
-                <thead>
-                  <tr>
-                  <th class="center">Oder Id</th>
-                  <th>Name</th>
-                  <th>SKU</th>
-                  <th class="right">Color</th>
-                  <th class="center">Qty</th>
-                  <th class="right">Item</th>
-                  <th class="right">Address</th>
-                  <th class="right">Mobile</th>
-                  <th class="right">Amount</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                  <td class="center">1</td>
-                  <td class="left strong"></td>
-                  <td class="left">
-                    <table>
-                      <tr>
-                        <td> </td>
-                      </tr>
-               
-                    </table>
-                  </td>
+<h1>Company Name : Bigshot</h1>
+<p>Company Email</p>
+<p>Company Address : Uttara</p>
+<table>
+  <tr>
+    <th>Oder Id</th>
+    <th>Name</th>
+    <th>SKU</th>
+    <th>Color</th>
+    <th>Qty</th>
+    <th>Item</th>
+    <th>Address</th>
+    <th>Mobile</th>
+    <th>Amount</th>
+  </tr>
+  <tr>
+    <td>Barcode &Create date</td>
+    <td>Saiful</td>
+    <td>Sku</td>
+    <td>Red</td>
+    <td>1</td>
+    <td>Mobile</td>
+    <td>Uttara</td>
+    <td>32456784546</td>
+    <td>2300</td>
+  </tr>
 
-                  <td class="right">
-                    <table>
-
-                      <tr>
-                        <td>Red</td>
-                      </tr>
-                 
-                    </table>
-                  </td>
-                  <td class="center">
-                    <table>
-
-                      <tr>
-                        <td>5</td>
-                      </tr>
-                      
-                    </table>
-                  </td>
-                  <td class="right"><table>
-
-                      <tr>
-                        <td>mobile</td>
-                      </tr>
-
-                    </table></td>
-                  <td class="right">uttara</td>
-                  <td class="right">132343546578</td>
-                  <td class="right">243354</td>
-
-                  </tr>
-               
-              
-                </tbody>
-               
-              </table>
-            </div>
-
-       
-
-          </div>
-        </div>
-      </div>
-<button onclick="window.print()">Print this page</button>
-    </section>
-
-      </div>
-   
-
-@endsection
-
-@section('js')
-
-@endsection
+</table>
+<script type="text/javascript">
+  window.print();
+</script>
+</body>
+</html>
