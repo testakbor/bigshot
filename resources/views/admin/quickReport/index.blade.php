aa8a120479797e922893f3e862c6c18b2622ba03
saifulislamsappu <saifulislam3844@gmail.com>
2020-09-24 13:23:30 +0600

    quick report page design

--- resources/views/admin/quickReport/index.blade.php
+++ resources/views/admin/quickReport/index.blade.php
@@ -93,7 +93,7 @@
                 <div class="box" style="background: #3467C1">
                   <!-- <i class="fa fa-lemon ml-1"></i> -->
                  
-                  <h3 class="text-center">Pending Order</h3>
+                  <h3 class="text-center">Dispatch</h3>
 
                 </div>
               </div>
@@ -163,7 +163,7 @@
                 <div class="box" style="background: #3467C1">
                   <!-- <i class="fa fa-lemon ml-1"></i> -->
                  
-                  <h3 class="text-center">Pending Order</h3>
+                  <h3 class="text-center">Stock Moving</h3>
 
                 </div>
               </div>
@@ -172,7 +172,7 @@
                   <!-- <i class="fa fa-user ml-1"></i> -->
                  
                  
-                  <h3 class="text-center">Today </h3>
+                  <h3 class="text-center">Week 1 </h3>
                  
                   <p class="lead text-center font-weight-bold">5</p>
                 </div>
@@ -182,7 +182,7 @@
                   <!-- <i class="fa fa-handshake ml-1"></i> -->
                   
                  
-                  <h3 class="text-center"> Day 1</h3>
+                  <h3 class="text-center"> Week 2</h3>
                   
                   <p class="lead text-center font-weight-bold">2</p>
                 </div>
@@ -192,7 +192,7 @@
                   <!-- <i class="fa fa-handshake ml-1"></i> -->
                   
                  
-                  <h3 class="text-center"> Day 2</h3>
+                  <h3 class="text-center"> Month 1</h3>
                   
                   <p class="lead text-center font-weight-bold">0</p>
                 </div>
@@ -202,7 +202,7 @@
                   <!-- <i class="fa fa-handshake ml-1"></i> -->
                   
                  
-                  <h3 class="text-center"> Day 3</h3>
+                  <h3 class="text-center"> Month 2</h3>
                   
                   <p class="lead text-center font-weight-bold">10</p>
                 </div>
@@ -212,7 +212,7 @@
                   <!-- <i class="fa fa-handshake ml-1"></i> -->
                   
                  
-                  <h3 class="text-center"> Day 4</h3>
+                  <h3 class="text-center"> Month 3</h3>
                   
                   <p class="lead text-center font-weight-bold">5</p>
                 </div>
@@ -227,7 +227,187 @@
                   <p class="lead text-center font-weight-bold">0</p>
                 </div>
               </div>
-        </div>       
+        </div>
+        <div class="row ml-2 mt-2">
+           <div class="col-1">
+                <div class="box" style="background: #3467C1">
+                  <!-- <i class="fa fa-lemon ml-1"></i> -->
+                 
+                  <h3 class="text-center">Stock</h3>
+
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box" style="background: #68A93B">
+                  <!-- <i class="fa fa-user ml-1"></i> -->
+                 
+                 
+                  <h3 class="text-center">Pieces </h3>
+                 
+                  <p class="lead text-center font-weight-bold">5</p>
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box bg-warning">
+                  <!-- <i class="fa fa-handshake ml-1"></i> -->
+                  
+                 
+                  <h3 class="text-center"> Amount</h3>
+                  
+                  <p class="lead text-center font-weight-bold">2345450</p>
+                </div>
+              </div>                 
+        </div>
+        <div class="row ml-2 mt-2">
+           <div class="col-1">
+                <div class="box" style="background: #3467C1">
+                  <!-- <i class="fa fa-lemon ml-1"></i> -->
+                 
+                  <h3 class="text-center">Lower Stock</h3>
+
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box" style="background: #68A93B">
+                  <!-- <i class="fa fa-user ml-1"></i> -->
+                 
+                 
+                  <h3 class="text-center">Weekly </h3>
+                 
+                  <p class="lead text-center font-weight-bold">5</p>
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box bg-warning">
+                  <!-- <i class="fa fa-handshake ml-1"></i> -->
+                  
+                 
+                  <h3 class="text-center"> All</h3>
+                  
+                  <p class="lead text-center font-weight-bold">2345450</p>
+                </div>
+              </div>                 
+        </div> 
+        <div class="row ml-2 mt-2">
+           <div class="col-1">
+                <div class="box" style="background: #3467C1">
+                  <!-- <i class="fa fa-lemon ml-1"></i> -->
+                 
+                  <h3 class="text-center">Sold Out</h3>
+
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box" style="background: #68A93B">
+                  <!-- <i class="fa fa-user ml-1"></i> -->
+                 
+                 
+                  <h3 class="text-center">Weekly</h3>
+                 
+                  <p class="lead text-center font-weight-bold">5</p>
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box bg-warning">
+                  <!-- <i class="fa fa-handshake ml-1"></i> -->
+                  
+                 
+                  <h3 class="text-center"> Yearly</h3>
+                  
+                  <p class="lead text-center font-weight-bold">300</p>
+                </div>
+              </div>                 
+        </div>
+        <div class="row ml-2 mt-2">
+           <div class="col-1">
+                <div class="box" style="background: #3467C1">
+                  <!-- <i class="fa fa-lemon ml-1"></i> -->
+                 
+                  <h3 class="text-center">Best Selling</h3>
+
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box" style="background: #68A93B">
+                  <!-- <i class="fa fa-user ml-1"></i> -->
+                 
+                 
+                  <h3 class="text-center">Weekly</h3>
+                 
+                  <p class="lead text-center font-weight-bold">5</p>
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box " style="background: #68A93B">
+                  <!-- <i class="fa fa-handshake ml-1"></i> -->
+                  
+                 
+                  <h3 class="text-center"> Yearly</h3>
+                  
+                  <p class="lead text-center font-weight-bold">300</p>
+                </div>
+              </div>                 
+        </div>  
+        <div class="row ml-2 mt-2">
+           <div class="col-1">
+                <div class="box" style="background: #3467C1">
+                  <!-- <i class="fa fa-lemon ml-1"></i> -->
+                 
+                  <h3 class="text-center">Delivered</h3>
+
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box" style="background: #68A93B">
+                  <!-- <i class="fa fa-user ml-1"></i> -->
+                 
+                 
+                  <h3 class="text-center">Quantity</h3>
+                 
+                  <p class="lead text-center font-weight-bold">5</p>
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box bg-warning">
+                  <!-- <i class="fa fa-handshake ml-1"></i> -->
+                  
+                 
+                  <h3 class="text-center"> Amount</h3>
+                  
+                  <p class="lead text-center font-weight-bold">300</p>
+                </div>
+              </div>                 
+        </div>   
+        <div class="row ml-2 mt-2">
+           <div class="col-1">
+                <div class="box" style="background: #3467C1">
+                  <!-- <i class="fa fa-lemon ml-1"></i> -->
+                 
+                  <h3 class="text-center">Gross Profit</h3>
+
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box" style="background: #68A93B">
+                  <!-- <i class="fa fa-user ml-1"></i> -->
+                 
+                 
+                  <h3 class="text-center">Monthly</h3>
+                 
+                  <p class="lead text-center font-weight-bold">500000</p>
+                </div>
+              </div>
+              <div class="col-1 ml-4">
+                <div class="box bg-warning">
+                  <!-- <i class="fa fa-handshake ml-1"></i> -->
+                  
+                 
+                  <h3 class="text-center"> Yearly</h3>
+                  
+                  <p class="lead text-center font-weight-bold">300</p>
+                </div>
+              </div>                 
+        </div>     
             
 
   
