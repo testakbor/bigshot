8cfa763f83c376752d17009031420963cad02c72
parthokar90 <parthokar90@gmail.com>
2020-09-29 09:53:17 +0600

    c

--- resources/views/admin/order/list.blade.php
+++ resources/views/admin/order/list.blade.php
@@ -1,93 +1,96 @@
 @extends('admin.layouts.master')
 @section('content')
 <div class="content-wrapper" style="min-height: 1203.6px;">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Order List</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{route('admin.home')}}">Home</a></li>
-              <li class="breadcrumb-item active">Order History</li>
-            </ol>
-          </div>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Order List</h1>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{route('admin.home')}}">Home</a></li>
+            <li class="breadcrumb-item active">Order History</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        @include('admin.includes.messages')
-        <div class="row">
-           
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Order</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table class="table table-bordered table-striped">
-                  <thead class="bg-info">                  
-                    <tr>
-                      <th style="width: 10px">#</th>
-                      <th>Order</th>
-                      <th>Date</th>
-                      <th>Status</th>
-                      <!-- <th>Total</th> -->
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @php 
-                    $i=1;
-                    @endphp
-                   @foreach($orders as $value)
-                   @foreach ($value->productMeta as $meta)
-                   @if($meta['meta_key']=='_order_total')
-                   @php                            
-                   $total=$meta['meta_value'];
-                   @endphp
-                 @endif
-                   
-                   @endforeach
-                    <tr >                     
-                      <td>{{$i}}</td>
-                      <td>{{$value->post_name}}</td>
-                      <td>{{$value->post_date}}</td>
-                      <td>{{$value->post_status}}</td>
-                      <!-- <td>{{$value->total}}</td> -->
-                      <td><a class="btn btn-success" href="{{route('order.edit',$value->ID)}}">Change Status</a></td>
-                    </tr>
-                    @php 
-                    $i++;
-                    @endphp
-                   @endforeach
-                  </tbody>
-                </table>
-              </div>
-              <div class="d-flex justify-content-center">            
-                {{$orders->links()}}                  
-              </div>  
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      @include('admin.includes.messages')
+      <div class="row">
+
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Order</h3>
             </div>
-            <!-- /.card -->
-           
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table class="table table-bordered table-striped">
+                <thead class="bg-info">
+                  <tr>
+                    <th style="width: 10px">#</th>
+                    <th>Order</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <!-- <th>Total</th> -->
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @php
+                  $i=1;
+                  @endphp
+                  @foreach($orders as $value)
+                  @foreach ($value->productMeta as $meta)
+                  @if($meta['meta_key']=='_order_total')
+                  @php
+                  $total=$meta['meta_value'];
+                  @endphp
+                  @endif
 
+                  @endforeach
+                  <tr>
+                    <td>{{$i}}</td>
+                    <td>{{$value->post_name}}</td>
+                    <td>{{$value->post_date}}</td>
+                    <td>{{$value->post_status}}</td>
+                    <!-- <td>{{$value->total}}</td> -->
+                    <td>
+                      <a class="btn btn-success btn-sm" href="{{route('order.edit',$value->ID)}}">Change Status</a>
+                      <a class="btn btn-primary btn-sm" href="{{route('download.shipping.address',$value->ID)}}">Download Shipping Address</a>
+                    </td>
+                  </tr>
+                  @php
+                  $i++;
+                  @endphp
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+            <div class="d-flex justify-content-center">
+              {{$orders->links()}}
+            </div>
           </div>
-        
+          <!-- /.card -->
 
-          </div>
-          <!-- /.col -->
+
         </div>
-      
-      
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
+
+
+      </div>
+      <!-- /.col -->
+    </div>
+
+
+    <!-- /.row -->
+</div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+</div>
 @endsection
---
+++ resources/views/admin/pdf/order/shipping_address.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        #invoice-POS {
+            box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
+            padding: 2mm;
+            margin: 0 auto;
+            width: 140mm;
+            background: #FFF;
+            min-height: 400px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="invoice-POS">
+
+        <center id="top">
+            <div class="logo"></div>
+            <div class="info">
+                <h2>BiGshot</h2>
+            </div>
+            <!--End Info-->
+        </center>
+        <!--End InvoiceTop-->
+
+        <div class="col-md-12 row mt-5">
+            <div class="col-md-6">
+                <h4>Shipping Address</h4>
+                <p> Riaz Howlader
+                    House 15, Sector –10</br>
+                    Road –4, Uttara-Dhaka.</br>
+                    Phone : 017167474747</br>
+                </p>
+            </div>
+
+            <div class="col-md-6">
+                <h4>Total Quantity: 2</h4>
+                <p> Total Due: 2,000tk
+                </p>
+            </div>
+
+        </div>
+        <div class="col-md-12">
+            <h2 style="text-align: center;">Barcode or QR code</h2>
+        </div>
+
+    </div>
+
+
+    <!-- <script type="text/javascript">
+  window.print();
+</script> -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
