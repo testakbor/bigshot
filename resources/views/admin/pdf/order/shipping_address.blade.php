ff3a2e1da7f519770d7eb044dc1649282c99b565
saifulislamsappu <saifulislam3844@gmail.com>
2020-09-29 10:52:58 +0600

    pdf page

--- resources/views/admin/pdf/order/shipping_address.blade.php
+++ resources/views/admin/pdf/order/shipping_address.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   
     <style>
         #invoice-POS {
             box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
@@ -28,37 +28,27 @@
             <!--End Info-->
         </center>
         <!--End InvoiceTop-->
-
-        <div class="col-md-12 row mt-5">
-            <div class="col-md-6">
+       <div style="width: 100%;">
+            <div style="float: left;width: 50%;">
                 <h4>Shipping Address</h4>
                 <p> Riaz Howlader
                     House 15, Sector –10</br>
                     Road –4, Uttara-Dhaka.</br>
                     Phone : 017167474747</br>
-                </p>
+                </p>  
             </div>
-
-            <div class="col-md-6">
+            <div style="float: right;width: 50%;">
                 <h4>Total Quantity: 2</h4>
                 <p> Total Due: 2,000tk
                 </p>
             </div>
-
-        </div>
-        <div class="col-md-12">
-            <h2 style="text-align: center;">Barcode or QR code</h2>
-        </div>
+       </div>
+       <div style="width: 100%; float: left;">
+           <h2 style="text-align: center;">Barcode or QR code</h2>
+       </div>
 
     </div>
 
-
-    <!-- <script type="text/javascript">
-  window.print();
-</script> -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
