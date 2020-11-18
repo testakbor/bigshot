8d9f70cab247c0ccf722d05ced3ec4d869012e20
Akbor hossain <testakbor@gmail.com>
2020-11-18 16:21:59 +0600

    order list

--- resources/views/front/order/list.blade.php
+++ resources/views/front/order/list.blade.php
@@ -1,91 +1,75 @@
 
 @extends('front.layouts.front_master')
 @section('content')
+<style>
+    table tr td{
+        font-size: 0.8em;
+    }
+</style>
 <!-- Page Content  -->
-<div class="container">
-      @include('admin.includes.messages')
-   <h4 class="text-center">My Orders List</h4>
+<div class="container p-0">
+    @include('admin.includes.messages')
+    <h4 class="text-center">My Orders List</h4>
 
     <div class="table-responsive">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th class="center">Id</th>
-                <th class="center">Date</th>
-                <th class="right">Item</th>
-                <th class="right">Quantity</th>
-                <th class="right">Status</th>
-                <th class="right">Action</th>
-                <!-- <th class="right">Comments</th> -->
-              </tr>
-            </thead>
+        <table class="table table-bordered table-striped">           
             <tbody>
+                @foreach($shop_order as $key=>$order)
+                <tr class="bg-light text-center">
 
-                 @php $id=0; $qty=0; @endphp
-                 @foreach($shop_order as $key=>$order)
-                   <tr>
-                     <td>{{$order->ID}}</td>
-                     <td>Order placed: {{date('d-M-Y',strtotime($order->post_date))}}</td>
-                     <td>
-                      @foreach($order->orderItem as $item)
-                      
-                      @foreach($item->orderMeta as $meta)
-                        @if($meta->meta_key=='_product_id')
-                           @php $id=$meta->meta_value; @endphp
-                        @endif
-                      @endforeach
+                    <th>
+                        Order placed: <br>
+                        {{date('d-M-Y',strtotime($order->post_date))}}
+                    </th>
+                    <th>
+                        Processing <br>
+                        18-Nov-2020
+                    </th>
+                    <th>
+                        <a title="view invoice" href="{{route('customer_ordere_cancel',$order->ID)}}">
+                            Cancel <br>
+                            Return
+                        </a>
+                    </th>
+                    <th>
+                        <a title="view invoice" href="{{route('customer_ordere_edit',$order->ID)}}"> Details</i></a>                      
+                    </th>
+                </tr>  
+                @foreach($order->orderItem as $item)
+                @foreach($item->orderMeta as $meta)
+                @if($meta->meta_key=='_product_id')
+                @php $id=$meta->meta_value; @endphp
+                @endif
+                @if($meta->meta_key=='_qty')
+                @php $qty=$meta->meta_value; @endphp
+                @endif
+                @endforeach
+                <tr class="text-center">
+                    <td>
+
+                        @php $product_img=DB::table('postmeta')->where('post_id',$id)->where('meta_key','attached_file')->first(); @endphp
+                        <img width="50px" height="50px" src="{{asset('backend/products/'.$product_img->meta_value)}}">
+                        <br>
+                        @php $product_sku=DB::table('postmeta')->where('post_id',$id)->where('meta_key','_sku')->first(); @endphp
+                        {{$product_sku->meta_value}}
+                    </td>
+                    <td>
+                        {{$item->order_item_name}}
+                    </td>
+                    <td>
+                        {{$qty}}
+                    </td>
+                    <td>
+                    </td>
+                </tr>
+                @endforeach
+                @endforeach
 
 
 
-                       <table class="table">
-                        <tbody>
-                          <tr>
-                            <td style="border: 0px solid #ffffff;">
-                              {{$item->order_item_name}}<br> 
-                               @php $product_sku=DB::table('postmeta')->where('post_id',$id)->where('meta_key','_sku')->first(); @endphp
-                              {{$product_sku->meta_value}}
-                            </td>
-                            <td style="border: 0px solid #ffffff;">
-                               @php $product_img=DB::table('postmeta')->where('post_id',$id)->where('meta_key','attached_file')->first(); @endphp
-                              <img width="50px" height="50px" src="{{asset('backend/products/'.$product_img->meta_value)}}">
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                        @endforeach
-                     </td>
-
-
-                     <td>
-                        @foreach($order->orderItem as $item)
-                        @foreach($item->orderMeta as $meta)
-                        @if($meta->meta_key=='_qty')
-                          @php $qty=$meta->meta_value; @endphp
-                        @endif
-                        @endforeach
-                       <table class="table">
-                        <tbody>
-                          <tr>
-                            <td style="border: 0px solid #ffffff;">{{$qty}}</td>
-                            <td style="border: 0px solid #ffffff;">Pc's</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                        @endforeach
-                     </td>
-                     <td>{{$order->post_status}}</td>
-                     <td>
-                        <a title="view invoice" class="btn btn-success btn-sm" href="{{route('customer_ordere_edit',$order->ID)}}"><i class="fa fa-eye"></i></a>
-                        @if($order->post_status=='cancel')
-                          @else 
-                        <a title="cancel or return order" class="btn btn-danger btn-sm" href="{{route('customer_ordere_cancel',$order->ID)}}"><i class="fa fa-times"></i></a>
-                        @endif 
-                     </td>
-                   </tr>  
-                  @endforeach
             </tbody>
-      </table>
-  {{$shop_order->links()}}
-</div>
+        </table>
+        {{$shop_order->links()}}
+    </div>
 </div>
 @endsection
