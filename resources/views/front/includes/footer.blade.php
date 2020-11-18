57780c89e2535a7c88e7163eed189bd48e6efce2
Akbor hossain <testakbor@gmail.com>
2020-11-18 14:12:22 +0600

    info

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -3,9 +3,9 @@
 			<div> <a href="{{route('terms.conditions')}}" class="btn btn-default" >Terms & Conditions</a> </div>
 			<div> <a href="{{route('return.policy')}}" class="btn btn-default" >Return Policy</a> </div>
 			<div><a href="https://www.facebook.com/bigshotstyle/" class="btn btn-default"> Follow us <i class="fab fa-facebook-f"></i></a></div>
-			<div class="btn"> Payment Option </div>
-			<div class="btn ml-0 pl-0"> <i style="font-size: 30px;" class="fa fa-money-bill-alt"></i></div>
-			<div class="btn ml-0 pl-0"> <img width="35px" height="25px" src="{{asset('assets/front/payment/bkash.jpg')}}"> </div>
-			<div class="btn ml-0 pl-0"> <i style="font-size: 30px;" class="fab fa-cc-visa"></i> </div>
-			<div class="btn ml-0 pl-0"> <i style="font-size: 30px;" class="fab fa-cc-mastercard"></i> </div>
+			<div class="btn pl-0 pr-1"> Payment Option </div>
+			<div class="btn ml-0 pl-0 pr-1"> <i style="font-size: 30px;" class="fa fa-money-bill-alt"></i></div>
+			<div class="btn ml-0 pl-0 pr-1"> <img width="35px" height="25px" src="{{asset('assets/front/payment/bkash.jpg')}}"> </div>
+			<div class="btn ml-0 pl-0 pr-1"> <i style="font-size: 30px;" class="fab fa-cc-visa"></i> </div>
+			<div class="btn ml-0 pl-0 pr-0"> <i style="font-size: 30px;" class="fab fa-cc-mastercard"></i> </div>
 		</div>
