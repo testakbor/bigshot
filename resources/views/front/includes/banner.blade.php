4e7059c72d6359cdab8c152e545e881c8b1efad2
parthokar90 <parthokar90@gmail.com>
2020-11-19 13:41:47 +0600

    some change

--- resources/views/front/includes/banner.blade.php
+++ resources/views/front/includes/banner.blade.php
@@ -1,10 +1,12 @@
-        @php
-           $banner=DB::table('posts')
-          ->where('post_type','banner')
-          ->join('postmeta','posts.ID','=','postmeta.post_id')
-          ->first();
-        @endphp
-        @if(isset($banner))
-        @if($banner->meta_key=='banner_image') @php $img=$banner->meta_value; @endphp @endif
-            <img src="{{asset('backend/banner/'.$img)}" width="1267" class="img-responsive img-fluid" alt="Responsive image" width="1267">
-        @endif
+ <div class="banner">
+   @php
+   $banner=DB::table('posts')
+   ->where('post_type','banner')
+   ->join('postmeta','posts.ID','=','postmeta.post_id')
+   ->first();
+   @endphp
+   @if(isset($banner) && $banner->meta_key=='banner_image')
+   @php $img=$banner->meta_value; @endphp 
+   <img src="{{asset('backend/banner/'.$img)}}" class="img-fluid" alt="Responsive image">
+   @endif
+</div>
